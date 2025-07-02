766d8af5b2d6acdfaeccf0680a749b38264487b4
Marc Nomena <89481372+MarcNomena@users.noreply.github.com>
2025-07-02 16:57:50 +0200

    fix layout

--- hackaton-estiam-tracking/app/home/(tabs)/_layout.tsx
+++ hackaton-estiam-tracking/app/home/(tabs)/_layout.tsx
@@ -1,11 +1,17 @@
 import { Tabs } from 'expo-router';
-
+import { Icon } from '@rneui/themed';
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home',headerShown:false }} />
-      <Tabs.Screen name="result" options={{ title: 'Result' }} />
-      <Tabs.Screen name="calendar" options={{ title: 'Calendar' }} />
+      <Tabs.Screen name="index" options={{ title: 'Accueil',headerShown:false, tabBarIcon: ({ color, size }) => (
+            <Icon name="home" type="material" color={color} size={size} />
+          ), }} />
+      <Tabs.Screen name="result" options={{ title: 'Notes',headerShown:false , tabBarIcon: ({ color, size }) => (
+            <Icon name="assessment" type="material" color={color} size={size} />
+          ), }} />
+      <Tabs.Screen name="calendar" options={{ title: 'Calendar',headerShown: false, tabBarIcon: ({ color, size }) => (
+            <Icon name="calendar-today" type="material" color={color} size={size} />
+          ), }} />
     </Tabs>
   );
 }
